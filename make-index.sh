abcaed051b9b0f4696d4c3b5f0102c147f8e619a
Samuel Wallace <swalla24@uic.edu>
2022-11-22 19:57:06 -0600

    make-index script complete

--- make-index.sh
+++ make-index.sh
@@ -1,12 +1,24 @@
-**Sam's RPG Workshop**
+#!/usr/bin/oil
 
-Samuel Wallace
 
- - 2022-11-22+19:52:48.2012824110 [.](.index.md.html)
+const title = $'**Sam\'s RPG Workshop**\n\n'
+
+const author = $'Samuel Wallace\n\n'
+
+const files = "$(find . -type f -name '*.md.html' -printf ' - %A+ [%h](%h%f)\n' | sort -r)"
+
+const footer = '''
+
 
 
 
 <link rel="stylesheet" href="./rpg-styles.css">
 
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
+'''
 
+
+const full = title ++ author ++ files ++ footer
+
+
+echo $full  > ./index.html
