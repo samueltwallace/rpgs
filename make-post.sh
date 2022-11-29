b5c7c2a0658c759576e71fce4a448e73f815016f
Samuel Wallace <swalla24@uic.edu>
2022-11-29 11:19:17 -0500

    start mist

--- make-post.sh
+++ make-post.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/oil
 
 
-const title = '**' ++ join($@) ++ '**' ++ $'\n\n\n\n\n'
+const title = '**' ++ "TBD" ++ '**' ++ $'\n\n\n\n\n'
 
 
 
@@ -10,7 +10,7 @@ const footer = '''
 
 
 
-<link rel="stylesheet" href="./rpg-styles.css">
+<link rel="stylesheet" href="../rpg-styles.css">
 
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
@@ -19,4 +19,4 @@ const footer = '''
 const full = title ++ footer
 
 
-echo $full > "./posts/wip.md.html"
+echo $full > "./posts/$@.md.html"
