4fb07fb5764fe9509949acaa75067fcd35b93600
Samuel Wallace <swalla24@uic.edu>
2022-12-28 09:37:20 -0600

    update dir with update log

--- make-index.sh
+++ make-index.sh
@@ -2,6 +2,8 @@
 
 const files =  "$(find ./posts/ -type f -name '*.md.html' -printf ' - %A+ [%f](%h/%f)\n' | sort -r)"
 
+const changes = "$(git log -n10 --format='%n%nAuthor: %an Updated: %ar%n%n%s%n%n Modified:%n' --name-only -- 'posts/*.md.html')"
+
 const footer = '''
 
 
@@ -9,6 +11,6 @@ const footer = '''
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
 
-const full = $'# Posts by Recently Updated\n' ++ files ++ footer
+const full = $'# Posts by Recently Updated\n' ++ files ++ $'\n\n# Article Change Log\n' ++ changes ++ footer
 
 echo $full > ./dir.md.html
