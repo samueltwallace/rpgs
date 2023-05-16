670eae1078716112fc9aacc9c27c74679ee803d9
Sam Wallace <samueltwallace@gmail.com>
2023-05-16 10:02:55 -0500

    add download link and adjust header positioning

--- make-post.sh
+++ make-post.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env oil
 
 
-const title = '**' ++ "TBD" ++ '**' ++ $'\n\n\n\n\n'
+const title = '**' ++ "TBD" ++ '**' ++ $'\n\n\n'
 
 
 
@@ -10,7 +10,13 @@ const footer = '''
 
 
 
-<link rel="stylesheet" href="../rpg-styles.css">
+
+
+
+
+[download this page](./$[join($@)].md.html?export)
+
+<link rel="stylesheet" href="https://rpgs.quajzen.page/rpg-styles.css">
 
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
