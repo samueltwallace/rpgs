bf0b968f0a5d9a41baaae957936e4e25d52a8558
Samuel Wallace <samueltwallace@gmail.com>
2023-06-07 15:29:05 -0500

    posts by first commit

--- make-index.sh
+++ make-index.sh
@@ -6,7 +6,7 @@ proc maketitle(@paths) {
     for file in (paths) {
 		     var firstline = "$(head -n 1 $file)"
 		     # var lastupdated = "$(stat -c '%x' $file)"
-		     var lastupdated = "$(git log --date=human --format='%cs' -- $file | head -n 1)"
+		     var lastupdated = "$(git log --date=human --format='%as' -- $file | tail -n 1)"
 		     var line = " - $lastupdated [" ++ firstline ++ '](' ++ file ++ $')\n'
 		     setvar bigline = bigline ++ line
 		 }
@@ -25,6 +25,6 @@ const footer = '''
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
 
-const full = $'# Posts by Recently Updated\n' ++ files ++ $'\n\n# Article Change Log\n' ++ changes ++ footer
+const full = $'# Posts by First Created\n' ++ files ++ $'\n\n# Article Change Log\n' ++ changes ++ footer
 
 echo $full > ./dir.md.html
