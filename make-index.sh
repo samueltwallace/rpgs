dcf9cb3a9f969a344f536c649c6058ca39c0ed96
Sam Wallace <samueltwallace@gmail.com>
2023-07-06 13:30:29 -0500

    get rid of git log

--- make-index.sh
+++ make-index.sh
@@ -16,8 +16,6 @@ proc maketitle(@paths) {
 
 const files =  "$(maketitle posts/**.md.html)"
 
-const changes = "$(git log -n10 --format='%n%nAuthor: %an Updated: %as%n%n%s%n%n Modified:%n' --name-only -- 'posts/*.md.html')"
-
 const footer = '''
 
 
@@ -25,6 +23,6 @@ const footer = '''
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
 
-const full = $'# Posts by First Created\n' ++ files ++ $'\n\n# Article Change Log\n' ++ changes ++ footer
+const full = $'# Posts by First Created\n' ++ files ++ $'\n\n# Article Change Log\n' ++ footer
 
 echo $full > ./dir.md.html
