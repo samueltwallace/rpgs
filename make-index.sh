4d37d9a212ed46969ed0b9593f9932f570581f24
Sam Wallace <samueltwallace@gmail.com>
2023-07-06 13:50:12 -0500

    fix dir.md.html

--- make-index.sh
+++ make-index.sh
@@ -23,6 +23,6 @@ const footer = '''
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="markdeep.min.js" charset="utf-8"></script><script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 '''
 
-const full = $'# Posts by First Created\n' ++ files ++ $'\n\n# Article Change Log\n' ++ footer
+const full = $'# Posts by First Created\n' ++ files ++ footer
 
 echo $full > ./dir.md.html
